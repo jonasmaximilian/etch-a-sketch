bc685ad6e5048b853b91956008e808300c7326a3
Jonas Senft-Raiß <jjonas@fab8b50c-1d16-44cd-afbd-4c03ed2079ca.fritz.box>
2022-04-03 01:08:19 +0200

    implemented function for creating 16x16 grid using DOM

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+const container = document.getElementById('container');
+
+function createSquare(rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+  };
+}
+
+createSquare(16, 16);
