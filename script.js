816a39a3d92e5bac199b0c3047df8215e380cbc1
Jonas Senft-Raiß <jjonas@Jonass-Air.fritz.box>
2022-04-03 15:08:01 +0200

    finished sketch logic

--- script.js
+++ script.js
@@ -7,17 +7,38 @@ function createSquare(rows, cols){
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
+        //cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
   };
 }
 
 createSquare(16, 16);
 
-const gridItems = document.querySelectorAll('.grid-item');
+let gridItems = document.querySelectorAll('.grid-item');
 
+/*
 gridItems.forEach(item => {
     item.addEventListener('mouseover', () => {
         item.style.backgroundColor = "black";
     });
+});
+*/
+
+document.addEventListener('mouseover', e => {
+    if(e.target.matches('.grid-item')){
+        e.target.style.backgroundColor = "black";
+    }
+})
+
+
+const reset = document.querySelector('#reset');
+
+
+reset.addEventListener('click', () => {
+    gridItems.forEach(item => {
+        item.remove();
+    });
+    let size = window.prompt("Enter size of new board: ")
+    createSquare(size, size);
+    gridItems = document.querySelectorAll('.grid-item');
 });
